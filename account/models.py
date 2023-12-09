620284333fda87dc13c2f7e458fc78af4c30a2be
YoonDii <dbswl9506@naver.com>
2023-12-09 23:56:47 +0900

    유저모델 수정

--- account/models.py
+++ account/models.py
@@ -1,8 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('유저는 이름이 필수 입니다.')
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        user = self.create_user(username, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser):
     idx = models.BigAutoField(primary_key=True)
 
@@ -16,3 +33,11 @@ class User(AbstractBaseUser):
     created_at = models.DateTimeField(help_text='생성일', verbose_name='date joined', auto_now_add=True)
     updated_at = models.DateTimeField(help_text='변경일', auto_now=True)
     last_login = models.DateTimeField(help_text='마지막 로그인', blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    object = UserManager()
+
+    def __str__(self):
+        return self.username
