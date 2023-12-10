9505da5d96c32b41621fa19510e8b1d2c992e50a
YoonDii <dbswl9506@naver.com>
2023-12-11 01:31:25 +0900

    모델수정

--- account/admin.py
+++ account/admin.py
@@ -1,13 +1,42 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User
 
 
-class CustomUserAdmin(UserAdmin):
-    model = User
-    list_display = ('username', 'is_staff', 'is_superuser')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+
+class UserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('username', 'is_staff', 'purchase_car', 'sale_car')
+    list_filter = ('is_staff',)
+
+    fieldsets = (  # 사용자 상세페이지에 나오는 값들 / 이름, 비밀번호 빼고 작성하지 않아도됨
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('purchase_car', 'sale_car')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+    )
+
+    add_fieldsets = (  # 사용자 생성 시 적을 값들
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username',)
+    ordering = ('username',)
     filter_horizontal = ()
 
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User, UserAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Car(models.Model):
-    name = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100)
+    name = models.CharField(help_text='차 이름',max_length=100)
+    brand = models.CharField(help_text='차 브랜드',max_length=100)
     vin = models.CharField(max_length=10, unique=True)
     base_price = models.IntegerField()
 
@@ -25,19 +25,19 @@ class Version(models.Model):
 
 class CarSales(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='sales')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchases')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='purchases')
     sale_date = models.DateField()
     sale_price = models.IntegerField()
 
 
 class UsedCarSales(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='used_sales')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='used_purchases')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='used_purchases')
     sale_date = models.DateField()
     sale_price = models.IntegerField()
     discount_rate = models.IntegerField()
 
-    def discount_price(self):
+    def discount_price(self):  # 할인된 가격 생성
         discounted_price = self.sale_price * (1 - (self.discount_rate / 100))
         return int(discounted_price)
 
